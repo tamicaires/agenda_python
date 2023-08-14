bc0bdf6022ee8b3f2b468ea3b0058237fbc83da5
tamicaires <tamiresdec@gmail.com>
2023-08-14 17:27:57 -0300

    Atualizações de funcionalidades, implementação de mais um campo denominado local e criação de funcionalidade de editar e excluir eventos

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
+    local = models.CharField(max_length=30)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,3 +19,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M horas')
+        
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
